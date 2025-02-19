96a3c08949b05aa3bf08de7ffd69561f0667f978
cfinotelli <carlos.finotelli@outlook.com>
2025-02-19 17:23:10 -0300

    feat:  imp[lement subscription and referral link route

--- src/routes/subscribe-to-event-route.ts
+++ src/routes/subscribe-to-event-route.ts
@@ -1,19 +1,21 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import z from 'zod'
+import { subscribeToEvent } from '../functions/subscribe-to-event'
 
 export const subscribeToEventRoute: FastifyPluginAsyncZod = async app => {
 	app.post(
 		'/subscriptions',
 		{
 			schema: {
+				summary: 'Subscribes someone to the event',
+				tags: ['subscription'],
 				body: z.object({
 					name: z.string(),
 					email: z.string().email(),
 				}),
 				response: {
 					201: z.object({
-						name: z.string(),
-						email: z.string().email(),
+						subscriberId: z.string(),
 					}),
 				},
 			},
@@ -21,10 +23,9 @@ export const subscribeToEventRoute: FastifyPluginAsyncZod = async app => {
 		async (request, reply) => {
 			const { name, email } = request.body
 
-			return reply.status(201).send({
-				name,
-				email,
-			})
+			const { subscriberId } = await subscribeToEvent({ name, email })
+
+			return reply.status(201).send({ subscriberId })
 		}
 	)
 }
